5908ad92a0616af8b55f2280001590bbde578cea
Viktor Blovytskyi <leito3101@gmail.com>
2016-04-05 12:03:23 +0300

    Initial commit

---
+++ scripts/stopwatch.js
@@ -0,0 +1,12 @@
+/**
+ * Created by leito on 05.04.2016.
+ */
+window.onload = function() {
+    new Vue({
+
+        el: '#stopwatch',
+        data: {
+            message: 'Test'
+        }
+    }); 
+};
