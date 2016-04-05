412a63b5808b09989207d79b199b67613ad6822e
Viktor Blovytskyi <leito3101@gmail.com>
2016-04-05 12:40:20 +0300

    Added method resetTime().

--- scripts/stopwatch.js
+++ scripts/stopwatch.js
@@ -2,11 +2,31 @@
  * Created by leito on 05.04.2016.
  */
 window.onload = function() {
-    new Vue({
+    var stopwatch = new Vue({
 
         el: '#stopwatch',
         data: {
-            message: 'Test'
+            time: '00:00:00.000',
+            timeBegan: null,
+            timeStopped: null,
+            started: null,
+            durationTime: 0
+        },
+        methods: {
+            /***
+             * This function starts 
+             */
+            startTime: function(){},
+            stopTime: function(){},
+            resetTime: function(){
+                clearInterval(this.started);
+                this.timeBegan = null;
+                this.timeStopped = null;
+                this.durationTime = 0;
+            },
+            displayTime: function(){}
+
+
         }
     }); 
 };
