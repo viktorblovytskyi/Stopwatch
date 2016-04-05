10d8ab1a1c75045bdb829ee768cce209a767e06c
Viktor Blovytskyi <leito3101@gmail.com>
2016-04-05 14:35:19 +0300

    Added new functions

--- scripts/stopwatch.js
+++ scripts/stopwatch.js
@@ -5,28 +5,76 @@ window.onload = function() {
     var stopwatch = new Vue({
 
         el: '#stopwatch',
+
         data: {
-            time: '00:00:00.000',
-            timeBegan: null,
-            timeStopped: null,
-            started: null,
-            durationTime: 0
+            time:         '00:00:00.0',
+            timeBegan:    null,
+            timeStopped:  null,
+            started:      null,
+            durationTime: 0,
+            hour:         0,
+            min:          0,
+            sec:          0,
+            msec:         0,
+            laps:         []
         },
+
         methods: {
+
             /***
-             * This function starts 
+             * This function starts stopwatch.
              */
-            startTime: function(){},
-            stopTime: function(){},
-            resetTime: function(){
+            startTime: function() {
+                if (this.timeBegan === null) {
+                    this.timeBegan = new Date();
+                } else if (this.timeBegan !== null) {
+                    this.durationTime += ( new Date() - this.timeStopped);
+                }
+
+                this.started = setInterval(this.displayTime(), 10);
+            },
+
+            /***
+             * This function stops stopwatch.
+             */
+            stopTime: function() {
+                this.timeStopped = new Date();
+                clearInterval(this.started);
+            },
+
+            /***
+             * This function reset data for stopwatch.
+             */
+            resetTime: function() {
                 clearInterval(this.started);
                 this.timeBegan = null;
                 this.timeStopped = null;
                 this.durationTime = 0;
             },
-            displayTime: function(){}
 
+            /***
+             * This function displays data for stopwatch.
+             *
+             */
+            displayTime: function() {
+                var currentTime = new Date(),
+                    elapsedTime = new Date(currentTime - this.timeBegan - this.timeStopped);
 
+                this.hour = elapsedTime.getUTCHours();
+                this.min = elapsedTime.getUTCMinutes();
+                this.sec = elapsedTime.getUTCSeconds();
+                this.msec = elapsedTime.getUTCMilliseconds();
+                this.generateStrTime();
+            },
+
+            /***
+             * This function generates string with time.
+             */
+            generateStrTime: function() {
+                this.time = (this.hour < 9 ? "0"+this.hour : this.hour) + ":"+
+                            (this.min < 9 ? "0"+this.min : this.min) + ":" +
+                            (this.sec < 9 ? "0"+this.sec : this.sec) + "." + this.msec;
+            }
         }
     }); 
 };
