f30b4db44867692198ad9bc916e3529c009714b3
Viktor Blovytskyi <leito3101@gmail.com>
2016-04-05 18:00:18 +0300

    Added new functions: addLapTime() and clearLaps().

--- scripts/stopwatch.js
+++ scripts/stopwatch.js
@@ -1,5 +1,5 @@
 /**
- * Created by leito on 05.04.2016.
+ * Created by Viktor Blvytskyi on 05.04.2016.
  */
 
 var stopwatch = new Vue({
@@ -11,11 +11,11 @@ var stopwatch = new Vue({
         timeStopped: null,
         started: null,
         durationTime: 0,
-        hour: 0,
-        min: 0,
-        sec: 0,
-        msec: 0,
-        laps: []
+        laps: [],
+        lastLap: 0,
+        elapsedTime: 0,
+        first_button: 'Start',
+        second_button: 'Reset'
     },
 
     methods: {
@@ -50,6 +50,7 @@ var stopwatch = new Vue({
             this.timeStopped = null;
             this.durationTime = 0;
             this.time = "00:00:00.0";
+            this.clearLaps();
         },
 
         /***
@@ -57,23 +58,55 @@ var stopwatch = new Vue({
          *
          */
         displayTime: function () {
-            var currentTime = new Date(),
-                elapsedTime = new Date(currentTime - this.timeBegan - this.timeStopped);
+            var currentTime = new Date();
 
-            this.hour = elapsedTime.getUTCHours();
-            this.min = elapsedTime.getUTCMinutes();
-            this.sec = elapsedTime.getUTCSeconds();
-            this.msec = elapsedTime.getUTCMilliseconds();
-            this.generateStrTime();
+            this.elapsedTime = new Date(currentTime - this.timeBegan - this.timeStopped);
+            this.time = this.generateStrTime(this.elapsedTime.getUTCHours(),
+                                             this.elapsedTime.getUTCMinutes(),
+                                             this.elapsedTime.getUTCSeconds(),
+                                             this.elapsedTime.getUTCMilliseconds());
         },
 
         /***
          * This function generates string with time.
+         * @param hour - int
+         * @param min - int
+         * @param sec - int
+         * @param msec - int
+         * @return String
          */
-        generateStrTime: function () {
-            this.time = (this.hour < 9 ? "0" + this.hour : this.hour) + ":" +
-                (this.min < 9 ? "0" + this.min : this.min) + ":" +
-                (this.sec < 9 ? "0" + this.sec : this.sec) + "." + this.msec;
+        generateStrTime: function (hour, min, sec, msec) {
+            msec = msec+"";
+            return (hour <= 9 ? "0" + hour : hour) + ":" +
+                (min <= 9 ? "0" + min : min) + ":" +
+                (sec <= 9 ? "0" + sec : sec) + "." + msec[0];
+        },
+
+        /***
+         * This function adds lap in list.
+         */
+        addLapTime: function () {
+            console.log('last '+ this.lastLap + ' now '+ this.elapsedTime);
+            if (this.lastLap === 0) {
+                this.laps.push({time: this.generateStrTime( this.elapsedTime.getUTCHours(),
+                                                            this.elapsedTime.getUTCMinutes(),
+                                                            this.elapsedTime.getUTCSeconds(),
+                                                            this.elapsedTime.getUTCMilliseconds())});
+            } else {
+                var lapTime = new Date(this.elapsedTime - this.lastLap);
+                this.laps.push({ time: this.generateStrTime( lapTime.getUTCHours(),
+                                                             lapTime.getUTCMinutes(),
+                                                             lapTime.getUTCSeconds(),
+                                                             lapTime.getUTCMilliseconds()) });
+            }
+            this.lastLap = this.elapsedTime;
+        },
+
+        /***
+         * This function deletes all data from array of laps.
+         */
+        clearLaps: function () {
+            this.laps = [];
         }
     }
 });
